521c649ccffe178a3cc7f6e2c785bf44e3ec13fa
Emanuel Jose dos Santos <emanueljs_ps@hotmail.com>
2022-03-02 16:34:45 -0400

    feat: create constants folder, files defalt_buttom, size_config and constants

---
+++ lib/app/core/constants.dart
@@ -0,0 +1,50 @@
+import 'package:dr_cash_clinic/app/core/size_config.dart';
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF0ad6e4);
+const kPrimaryLightColor = Color(0xFf28a0d9);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF0ad6e4), Color(0xFf28a0d9)],
+);
+const kSecondaryColor = Color(0xFF555763);
+const kTextColor = Color(0xFF000000);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+      EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
+}
