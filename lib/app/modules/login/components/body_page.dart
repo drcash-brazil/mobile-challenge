f5451640e8aa6f4a67516be9b96640f724d154a0
Emanuel Jose dos Santos <emanueljs_ps@hotmail.com>
2022-03-02 18:49:52 -0400

    feat: create login/component file bod_page

---
+++ lib/app/modules/login/components/body_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/no_account_text_page.dart';
+import '../../../components/social_card_page.dart';
+import '../../../core/size_config.dart';
+import 'login_form_page.dart';
+
+class BodyPage extends StatefulWidget {
+  final String title;
+  const BodyPage({Key? key, this.title = 'BodyPageDart'}) : super(key: key);
+  @override
+  BodyPageState createState() => BodyPageState();
+}
+
+class BodyPageState extends State<BodyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  "Sign in with your email and password  \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                const LoginFormPage(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCardPage(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocialCardPage(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialCardPage(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                const NoAccountTextPage(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
