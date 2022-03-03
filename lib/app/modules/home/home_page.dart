3acbec59766082f584fd81a9dedddc610ee7359f
Emanuel Jose dos Santos <emanueljs_ps@hotmail.com>
2022-03-03 14:28:58 -0400

    feat: compose layout homepage

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:dr_cash_clinic/app/components/default_button.dart';
+import 'package:dr_cash_clinic/app/core/constants.dart';
+import 'package:dr_cash_clinic/app/core/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_triple/flutter_triple.dart';
 import 'home_store.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,30 +16,128 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeStore> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter'),
+        //title: Icons.abc ,
+        actions: [Icon(Icons.settings), Icon(Icons.message)],
+        toolbarHeight: 70,
+        backgroundColor: const Color(0xFF06D6A0),
       ),
-      body: ScopedBuilder<HomeStore, Exception, int>(
-        store: store,
-        onState: (_, counter) {
-          return Padding(
-            padding: EdgeInsets.all(10),
-            child: Text('$counter'),
-          );
-        },
-        onError: (context, error) => Center(
-          child: Text(
-            'Too many clicks',
-            style: TextStyle(color: Colors.red),
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              Card(
+                color: const Color(0xFF06D6A0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.gps_fixed),
+                      title: Text('LUCAS',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold)),
+                      subtitle: Text('Dependente'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        TextButton(
+                          child: const Text(
+                            'MINHA LOCALIDADE',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {/* ... */},
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              DefaultButton(
+                press: () {},
+                text: 'Pesquisar Cidades',
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              DefaultButton(
+                press: () {},
+                text: 'Pesquisar Estado',
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      child: const Center(
+                          child: Text('Cobertura',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                      width: 160,
+                      height: 160,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      child: const Center(
+                          child: Text('Reembolso',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                      width: 160,
+                      height: 160,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      child: const Center(
+                          child: Text('Meu Perfil',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                      width: 160,
+                      height: 160,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      child: const Center(
+                          child: Text('Fale Conosco',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                      width: 160,
+                      height: 160,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                  child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.exit_to_app),
+              )),
+            ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          store.increment();
-        },
-        child: Icon(Icons.add),
       ),
     );
   }
