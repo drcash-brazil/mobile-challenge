a4039c92dbe68c07e3e2d93e12bbab0f552743fb
evertoneduardo <everton.pires@in8.com.br>
2022-03-19 14:00:11 -0300

    chore: improvements in to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+// TODO: call the firebase when its possible
+Future<void> main() async {
+  runZonedGuarded(() {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterError.onError = (FlutterErrorDetails errorDetails) {};
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+        .then((_) {
+      runApp(
+        ModularApp(
+          module: AppModule(),
+          child: const AppWidget(),
+        ),
+      );
+    });
+  }, (Object error, StackTrace stackTrace) {});
+}
