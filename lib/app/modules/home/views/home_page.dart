46856ad89be4586894d31373ad8880e11ab9ce65
evertoneduardo <everton.pires@in8.com.br>
2022-03-21 21:42:08 -0300

    chore: improvements in home page

--- lib/app/modules/home/views/home_page.dart
+++ lib/app/modules/home/views/home_page.dart
@@ -6,11 +6,11 @@ import '../../../modules/util/constants/icons_constants.dart';
 import '../../util/colors/colors.dart';
 import '../stores/clinics_store.dart';
 
-
 class HomePage extends StatefulWidget {
-  const HomePage(
-      {Key? key, required this.clinicsStore,})
-      : super(key: key);
+  const HomePage({
+    Key? key,
+    required this.clinicsStore,
+  }) : super(key: key);
 
   final ClinicsStore clinicsStore;
 
@@ -20,46 +20,39 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends ModularState<HomePage, ClinicsStore>
     with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-  late PageController _pageController;
-
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-    _pageController = PageController(initialPage: 0);
     widget.clinicsStore.getAllClinics(context: context);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    _pageController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: DefaultTabController(
-        length: 3,
-        initialIndex: 0,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * .01),
-              child: SvgPicture.asset(
-                IconConstant.logoWide,
-                color: ColorsConstants.white,
-                height: MediaQuery.of(context).size.height * .05,
-              ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * .01),
+            child: SvgPicture.asset(
+              IconConstant.logoWide,
+              color: ColorsConstants.white,
+              height: MediaQuery.of(context).size.height * .05,
             ),
-            backgroundColor: ColorsConstants.primary,
-            centerTitle: true,
           ),
-          body: Container(),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.logout,
+                  color: ColorsConstants.white,
+                  size: 18.0,
+                ))
+          ],
+          backgroundColor: ColorsConstants.primary,
+          centerTitle: true,
         ),
+        body: Container(),
       ),
     );
   }
--- lib/app/modules/util/constants/icons_constants.dart
+++ lib/app/modules/util/constants/icons_constants.dart
@@ -2,7 +2,9 @@ abstract class IconConstant {
 
   static String get logoColor => 'assets/icon/logo.svg';
 
-  static String get logoWide => 'assets/icon/logo.svg';
+  static String get logoWhite => 'assets/icon/logo_white.svg';
+
+  static String get logoWide => 'assets/icon/logo_wide.svg';
 
   //----------------------------------------------------------------------------
 
