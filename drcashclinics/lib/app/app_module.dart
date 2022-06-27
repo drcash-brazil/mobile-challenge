8259a15a0c936821147c21b7930f62effa4834ef
Billy <billy@appratico.com>
2022-06-27 04:11:10 -0300

    Main - App icon/Splash screen

--- drcashclinics/lib/app/app_module.dart
+++ drcashclinics/lib/app/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:drcashclinics/app/modules/home/home_module.dart';
 import 'package:drcashclinics/app/modules/login/login_module.dart';
+import 'package:drcashclinics/app/modules/splash_screen/splash_screen_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
@@ -8,7 +9,8 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: LoginModule()),
+    ModuleRoute('/', module: SplashScreenModule()),
+    ModuleRoute('/login/', module: LoginModule()),
     ModuleRoute('/home/', module: HomeModule()),
   ];
 }
