32031e98f86285c666e3274251b728670314e12c
Rick Ramos <rhbr@icomp.ufam.edu.br>
2022-06-26 22:02:07 -0400

    Criação de Utils do projeto

---
+++ mobile_challenge/lib/src/core/util/assets.dart
@@ -0,0 +1,7 @@
+class AppAssets{
+  static const String assetsFolder = 'assets';
+  static const String imagesFolder = '$assetsFolder/images';
+
+  static const String logoColor = '$imagesFolder/logo_drcash.png';
+  static const String logoWhite = '$imagesFolder/logo_drcash2.png';
+}
